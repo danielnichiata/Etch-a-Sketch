0187293f11e83a014fd63ef063ef452fbf39e5d3
daniel nichiata <danielnichiata@gmail.com>
2023-01-12 15:00:09 -0300

    add change size event button

--- script.js
+++ script.js
@@ -7,16 +7,32 @@ function createGridContainer() {
 
 gridContainer = createGridContainer();
 
-function createGridItems() {
-  for (let i = 0; i < 256; i++) {
+function createGridItems(gridSize) {
+  let gridItems = gridContainer.getElementsByClassName("grid-item");
+  while (gridItems.length > 0) {
+    gridContainer.removeChild(gridItems[0]);
+  }
+
+  for (let i = 0; i < gridSize*gridSize; i++) {
     let gridItem = document.createElement("div");
     gridItem.className = "grid-item";
     gridContainer.appendChild(gridItem);
     gridItem.addEventListener("mouseover", changeItemsColor);
   }
 }
-gridItem = createGridItems();
+
+createGridItems(16);
 
 function changeItemsColor() {
   this.style.backgroundColor = "black";
+}
+
+const bttnChangeSize = document.querySelector("button");
+bttnChangeSize.onclick = windowsAlert;
+
+function windowsAlert() {
+  const newGridSize = Number(prompt("Insira um tamanho para o grid?"));
+  createGridItems(newGridSize);
+  gridContainer.style.gridTemplateColumns = `repeat(${newGridSize}, 2fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${newGridSize}, 2fr)`;
 }
